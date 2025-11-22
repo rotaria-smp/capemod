c47c5893a46fe3e3cf22833e4e4dbbbbfb161534
Emil Mikkelsen Båge <84081233+EmilMikkelsenB@users.noreply.github.com>
2025-11-22 22:53:44 +0100

    remove narrator import

--- src/main/java/dev/thestaticvoid/capejs/Commands/CommandInit.java
+++ src/main/java/dev/thestaticvoid/capejs/Commands/CommandInit.java
@@ -4,7 +4,6 @@ import dev.thestaticvoid.capejs.Commands.CapeCommands;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import static com.mojang.text2speech.Narrator.LOGGER;
 import static dev.thestaticvoid.capejs.CapeJS.MOD_ID;
 
 @EventBusSubscriber(modid = MOD_ID)
@@ -14,6 +13,5 @@ public final class CommandInit {
     public static void registerCommands(RegisterCommandsEvent event) {
         CapeCommands.register(event.getDispatcher());
         SwitchCape.register(event.getDispatcher());
-        LOGGER.info("Registered server commands.");
     }
 }
